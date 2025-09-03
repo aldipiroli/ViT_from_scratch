bb03e7d20e6ee5003903eda034edb75400dd0cdd
Aldi Piroli <aldipiroli@gmail.com>
2025-09-03 18:46:42 +0200

    updated msa conventions

--- vit/models/vit.py
+++ vit/models/vit.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import math
-
+import torch.nn.functional as F
 
 class ImagePatcher(nn.Module):
     def __init__(self, out_size=1, patch_size=4):
@@ -33,56 +33,87 @@ class ImagePatcherCNN(nn.Module):
 
 
 class SelfAttention(nn.Module):
-    def __init__(self, in_size=128, out_size=128):
+    def __init__(self, embed_size=128, n_heads=8):
         super(SelfAttention, self).__init__()
-        self.in_size = in_size
-        self.out_size = out_size
-        self.K = nn.Linear(self.in_size, self.out_size)
-        self.Q = nn.Linear(self.in_size, self.out_size)
-        self.V = nn.Linear(self.in_size, self.out_size)
+        self.embed_size = embed_size
+        self.n_heads = n_heads
+        self.inter_embed_size = embed_size // n_heads
 
-    def forward(self, x):
-        k = self.K(x)
-        q = self.Q(x)
-        v = self.V(x)
+        self.Ukqv = nn.Linear(self.embed_size, 3 * self.inter_embed_size)
+
+    def forward(self, z):
+        b,n = z.shape[0],z.shape[1]
+        kqv = self.Ukqv(z)
+        kqv = kqv.reshape(b,n,self.inter_embed_size,3)
+        k = kqv[..., 0]
+        q = kqv[..., 1]
+        v = kqv[..., 2]
 
         qk = torch.matmul(q, k.transpose(-2, -1))
-        attention = nn.functional.softmax(qk / math.sqrt(self.out_size), -1)
+        attention = nn.functional.softmax(qk / math.sqrt(self.inter_embed_size), -1)
         self_attention = torch.matmul(attention, v)
         return self_attention
 
 
 class MultiHeadSelfAttention(nn.Module):
-    def __init__(self, in_size=128, out_size=128, num_heads=3):
+    def __init__(self, embed_size=128, num_heads=8):
         super(MultiHeadSelfAttention, self).__init__()
-        self.in_size = in_size
-        self.out_size = out_size
+        self.embed_size = embed_size
         self.num_heads = num_heads
         self.attention_heads = torch.nn.ModuleList(
-            [SelfAttention(self.in_size, self.out_size // self.num_heads) for _ in range(self.num_heads)]
+            [SelfAttention(self.embed_size, self.num_heads) for _ in range(self.num_heads)]
         )
-        self.projection_matrix = nn.Linear((self.out_size // num_heads) * num_heads, self.out_size)
+        self.projection_matrix = nn.Linear((self.embed_size // num_heads) * num_heads, self.embed_size)
 
-    def forward(self, x):
+    def forward(self, z):
         all_self_attentions = []
         for attention_head in self.attention_heads:
-            z = attention_head(x)
-            all_self_attentions.append(z)
+            z_msa = attention_head(z)
+            all_self_attentions.append(z_msa)
         all_self_attentions = torch.cat(all_self_attentions, -1)
         z = self.projection_matrix(all_self_attentions)
         return z
 
+class ParallelMultiHeadSelfAttention(nn.Module):
+    def __init__(self, embed_size=128, num_heads=8):
+        super(ParallelMultiHeadSelfAttention, self).__init__()
+        self.num_heads = num_heads
+        self.embed_size = embed_size
+        self.intermediate_embed_size = embed_size // num_heads
+        self.kqv_projection = nn.Linear(embed_size, 3* num_heads * self.intermediate_embed_size)
+
+    def forward(self, z):
+        b, n = z.shape[0],z.shape[1]
+        kvq = self.kqv_projection(z)
+        kvq = kvq.reshape(b, n, self.num_heads, self.intermediate_embed_size, 3) # (batch, n_patches, n_heads, embed_size, 3)
+        kvq = kvq.permute(2, 0, 1, 3, 4)
+        k = kvq[..., 0] # (n_heads, batch, n_patches, embed_size)
+        v = kvq[..., 1]
+        q = kvq[..., 2]
+
+        qk = q @ k.transpose(3,2)
+        a = F.softmax(qk, -1) * self.intermediate_embed_size**-0.5
+        sa = a @ v # (n_heads, batch, n_patches, embed_size)
+        mhsa = sa.permute(1,2,0,3).reshape(b, n, self.num_heads*self.intermediate_embed_size) # aggregate heads
+        return mhsa
+
 
 class TransformerEncoder(nn.Module):
-    def __init__(self, embed_size=128, num_patches=16):
+    def __init__(self, embed_size=128, num_patches=16, num_heads=8, use_parallel=False):
         super(TransformerEncoder, self).__init__()
         self.embed_size = embed_size
         self.num_patches = num_patches
 
         self.ln = nn.LayerNorm(embed_size)
-        self.multi_head_self_attention = MultiHeadSelfAttention(
-            in_size=self.embed_size, out_size=self.embed_size, num_heads=3
-        )
+        if not use_parallel: 
+            self.multi_head_self_attention = MultiHeadSelfAttention(
+                embed_size=self.embed_size,  num_heads=num_heads
+            )
+        else:
+            self.multi_head_self_attention = ParallelMultiHeadSelfAttention(
+                embed_size=self.embed_size, num_heads=num_heads
+            )
+
         self.mlp = nn.Sequential(
             nn.Linear(self.embed_size, self.embed_size * 2),
             nn.GELU(),
@@ -111,6 +142,7 @@ class SimpleViT(nn.Module):
         self.embed_size = config["MODEL"]["embed_size"]
         self.num_encoder_blocks = config["MODEL"]["num_encoder_blocks"]
         self.num_classes = config["MODEL"]["num_classes"]
+        self.num_mha_heads = config["MODEL"]["num_mha_heads"]
 
         self.patch_embed_transform = nn.Linear(self.patch_dim, self.embed_size)
         self.positional_embeddings = nn.Parameter(
@@ -120,8 +152,8 @@ class SimpleViT(nn.Module):
 
         self.encoders = torch.nn.ModuleList(
             [
-                TransformerEncoder(embed_size=self.embed_size, num_patches=self.num_patches)
-                for i in range(self.num_encoder_blocks)
+                TransformerEncoder(embed_size=self.embed_size, num_patches=self.num_patches, num_heads=self.num_mha_heads)
+                for _ in range(self.num_encoder_blocks)
             ]
         )
         self.classifier = nn.Linear(self.embed_size, self.num_classes)
